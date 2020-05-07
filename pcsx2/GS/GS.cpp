f6551061df327e4611efe6e7c0651a817b28ee93
Tellow Krinkle <tellowkrinkle@gmail.com>
2020-05-06 22:32:08 -0500

    Rearrange revived feature workarounds in GLLoader

--- pcsx2/GS/GS.cpp
+++ pcsx2/GS/GS.cpp
@@ -1272,7 +1272,6 @@ void GSApp::Init()
 	m_default_configuration["override_GL_ARB_sparse_texture"]             = "-1";
 	m_default_configuration["override_GL_ARB_sparse_texture2"]            = "-1";
 	m_default_configuration["override_GL_ARB_texture_barrier"]            = "-1";
-	m_default_configuration["override_GL_KHR_debug"]                      = "-1";
 	m_default_configuration["override_GL_ARB_shading_language_420pack"]   = "-1";
 	m_default_configuration["override_GL_ARB_buffer_storage"]             = "-1";
 #ifdef GL_EXT_TEX_SUB_IMAGE
