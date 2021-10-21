138cb2e239e7267c924eca418dbd778d204a88e2
Connor McLaughlin <stenzek@gmail.com>
2021-10-21 18:45:14 +1000

    Frontend: Add VulkanHostDisplay

---
+++ pcsx2/Frontend/VulkanHostDisplay.cpp
@@ -0,0 +1,457 @@
+﻿#include "PrecompiledHeader.h"
+
+#include "VulkanHostDisplay.h"
+#include "common/Assertions.h"
+#include "common/Console.h"
+#include "common/ScopedGuard.h"
+#include "common/Vulkan/Builders.h"
+#include "common/Vulkan/Context.h"
+#include "common/Vulkan/ShaderCache.h"
+#include "common/Vulkan/StagingTexture.h"
+#include "common/Vulkan/StreamBuffer.h"
+#include "common/Vulkan/SwapChain.h"
+#include "common/Vulkan/Util.h"
+#include "imgui.h"
+#include "imgui_impl_vulkan.h"
+#include <array>
+
+static constexpr u32 SHADER_CACHE_VERSION = 1;
+
+class VulkanHostDisplayTexture : public HostDisplayTexture
+{
+public:
+	VulkanHostDisplayTexture(Vulkan::Texture texture, Vulkan::StagingTexture staging_texture)
+		: m_texture(std::move(texture))
+		, m_staging_texture(std::move(staging_texture))
+	{
+	}
+	~VulkanHostDisplayTexture() override = default;
+
+	void* GetHandle() const override { return const_cast<Vulkan::Texture*>(&m_texture); }
+	u32 GetWidth() const override { return m_texture.GetWidth(); }
+	u32 GetHeight() const override { return m_texture.GetHeight(); }
+	u32 GetLayers() const override { return m_texture.GetLayers(); }
+	u32 GetLevels() const override { return m_texture.GetLevels(); }
+	u32 GetSamples() const override { return m_texture.GetSamples(); }
+
+	const Vulkan::Texture& GetTexture() const { return m_texture; }
+	Vulkan::Texture& GetTexture() { return m_texture; }
+	Vulkan::StagingTexture& GetStagingTexture() { return m_staging_texture; }
+
+private:
+	Vulkan::Texture m_texture;
+	Vulkan::StagingTexture m_staging_texture;
+};
+
+VulkanHostDisplay::VulkanHostDisplay() = default;
+
+VulkanHostDisplay::~VulkanHostDisplay()
+{
+	pxAssertRel(!g_vulkan_context, "Context should have been destroyed by now");
+	pxAssertRel(!m_swap_chain, "Swap chain should have been destroyed by now");
+}
+
+HostDisplay::RenderAPI VulkanHostDisplay::GetRenderAPI() const
+{
+	return HostDisplay::RenderAPI::Vulkan;
+}
+
+void* VulkanHostDisplay::GetRenderDevice() const
+{
+	return nullptr;
+}
+
+void* VulkanHostDisplay::GetRenderContext() const
+{
+	return nullptr;
+}
+
+void* VulkanHostDisplay::GetRenderSurface() const
+{
+	return m_swap_chain.get();
+}
+
+bool VulkanHostDisplay::ChangeRenderWindow(const WindowInfo& new_wi)
+{
+	g_vulkan_context->WaitForGPUIdle();
+
+	if (new_wi.type == WindowInfo::Type::Surfaceless)
+	{
+		g_vulkan_context->ExecuteCommandBuffer(true);
+		m_swap_chain.reset();
+		m_window_info = new_wi;
+		return true;
+	}
+
+	// recreate surface in existing swap chain if it already exists
+	if (m_swap_chain)
+	{
+		if (m_swap_chain->RecreateSurface(new_wi))
+		{
+			m_window_info = m_swap_chain->GetWindowInfo();
+			return true;
+		}
+
+		m_swap_chain.reset();
+	}
+
+	WindowInfo wi_copy(new_wi);
+	VkSurfaceKHR surface = Vulkan::SwapChain::CreateVulkanSurface(g_vulkan_context->GetVulkanInstance(),
+		g_vulkan_context->GetPhysicalDevice(), &wi_copy);
+	if (surface == VK_NULL_HANDLE)
+	{
+		Console.Error("Failed to create new surface for swap chain");
+		return false;
+	}
+
+	m_swap_chain = Vulkan::SwapChain::Create(wi_copy, surface, false);
+	if (!m_swap_chain)
+	{
+		Console.Error("Failed to create swap chain");
+		Vulkan::SwapChain::DestroyVulkanSurface(g_vulkan_context->GetVulkanInstance(), &wi_copy, surface);
+		return false;
+	}
+
+	m_window_info = m_swap_chain->GetWindowInfo();
+	return true;
+}
+
+void VulkanHostDisplay::ResizeRenderWindow(s32 new_window_width, s32 new_window_height, float new_window_scale)
+{
+	g_vulkan_context->WaitForGPUIdle();
+
+	if (!m_swap_chain->ResizeSwapChain(new_window_width, new_window_height))
+		pxFailRel("Failed to resize swap chain");
+
+	m_window_info = m_swap_chain->GetWindowInfo();
+}
+
+bool VulkanHostDisplay::SupportsFullscreen() const
+{
+	return false;
+}
+
+bool VulkanHostDisplay::IsFullscreen()
+{
+	return false;
+}
+
+bool VulkanHostDisplay::SetFullscreen(bool fullscreen, u32 width, u32 height, float refresh_rate)
+{
+	return false;
+}
+
+HostDisplay::AdapterAndModeList VulkanHostDisplay::GetAdapterAndModeList()
+{
+	return StaticGetAdapterAndModeList(m_window_info.type != WindowInfo::Type::Surfaceless ? &m_window_info : nullptr);
+}
+
+void VulkanHostDisplay::DestroyRenderSurface()
+{
+	m_window_info = {};
+	g_vulkan_context->WaitForGPUIdle();
+	m_swap_chain.reset();
+}
+
+std::unique_ptr<HostDisplayTexture> VulkanHostDisplay::CreateTexture(u32 width, u32 height, u32 layers, u32 levels,
+	u32 samples, const void* data, u32 data_stride,
+	bool dynamic /* = false */)
+{
+	static constexpr VkFormat vk_format = VK_FORMAT_R8G8B8A8_UNORM;
+	static constexpr VkImageUsageFlags usage =
+		VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT;
+
+	Vulkan::Texture texture;
+	if (!texture.Create(width, height, levels, layers, vk_format, static_cast<VkSampleCountFlagBits>(samples),
+			(layers > 1) ? VK_IMAGE_VIEW_TYPE_2D_ARRAY : VK_IMAGE_VIEW_TYPE_2D, VK_IMAGE_TILING_OPTIMAL,
+			usage))
+	{
+		return {};
+	}
+
+	Vulkan::StagingTexture staging_texture;
+	if (data || dynamic)
+	{
+		if (!staging_texture.Create(dynamic ? Vulkan::StagingBuffer::Type::Mutable : Vulkan::StagingBuffer::Type::Upload,
+				vk_format, width, height))
+		{
+			return {};
+		}
+	}
+	const Vulkan::Util::DebugScope debugScope(g_vulkan_context->GetCurrentCommandBuffer(),
+		"VulkanHostDisplay::CreateTexture");
+	texture.TransitionToLayout(g_vulkan_context->GetCurrentCommandBuffer(), VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+
+	if (data)
+	{
+		staging_texture.WriteTexels(0, 0, width, height, data, data_stride);
+		staging_texture.CopyToTexture(g_vulkan_context->GetCurrentCommandBuffer(), 0, 0, texture, 0, 0, 0, 0, width,
+			height);
+	}
+	else
+	{
+		// clear it instead so we don't read uninitialized data (and keep the validation layer happy!)
+		static constexpr VkClearColorValue ccv = {};
+		static constexpr VkImageSubresourceRange isr = {VK_IMAGE_ASPECT_COLOR_BIT, 0u, 1u, 0u, 1u};
+		vkCmdClearColorImage(g_vulkan_context->GetCurrentCommandBuffer(), texture.GetImage(), texture.GetLayout(), &ccv, 1u,
+			&isr);
+	}
+
+	texture.TransitionToLayout(g_vulkan_context->GetCurrentCommandBuffer(), VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+	// don't need to keep the staging texture around if we're not dynamic
+	if (!dynamic)
+		staging_texture.Destroy(true);
+
+	return std::make_unique<VulkanHostDisplayTexture>(std::move(texture), std::move(staging_texture));
+}
+
+void VulkanHostDisplay::UpdateTexture(HostDisplayTexture* texture, u32 x, u32 y, u32 width, u32 height,
+	const void* data, u32 data_stride)
+{
+	VulkanHostDisplayTexture* vk_texture = static_cast<VulkanHostDisplayTexture*>(texture);
+
+	Vulkan::StagingTexture* staging_texture;
+	if (vk_texture->GetStagingTexture().IsValid())
+	{
+		staging_texture = &vk_texture->GetStagingTexture();
+	}
+	else
+	{
+		// TODO: This should use a stream buffer instead for speed.
+		if (m_upload_staging_texture.IsValid())
+			m_upload_staging_texture.Flush();
+
+		if ((m_upload_staging_texture.GetWidth() < width || m_upload_staging_texture.GetHeight() < height) &&
+			!m_upload_staging_texture.Create(Vulkan::StagingBuffer::Type::Upload, VK_FORMAT_R8G8B8A8_UNORM, width, height))
+		{
+			pxFailRel("Failed to create upload staging texture");
+		}
+
+		staging_texture = &m_upload_staging_texture;
+	}
+
+	staging_texture->WriteTexels(0, 0, width, height, data, data_stride);
+	staging_texture->CopyToTexture(0, 0, vk_texture->GetTexture(), x, y, 0, 0, width, height);
+}
+
+void VulkanHostDisplay::SetVSync(VsyncMode mode)
+{
+	if (!m_swap_chain)
+		return;
+
+	// This swap chain should not be used by the current buffer, thus safe to destroy.
+	g_vulkan_context->WaitForGPUIdle();
+	m_swap_chain->SetVSync(mode != VsyncMode::Off);
+}
+
+bool VulkanHostDisplay::CreateRenderDevice(const WindowInfo& wi, std::string_view adapter_name, bool debug_device)
+{
+	// debug_device = true;
+
+	WindowInfo local_wi(wi);
+	if (!Vulkan::Context::Create(adapter_name, &local_wi, &m_swap_chain, false, debug_device, debug_device))
+	{
+		Console.Error("Failed to create Vulkan context");
+		m_window_info = {};
+		return false;
+	}
+
+	m_window_info = m_swap_chain ? m_swap_chain->GetWindowInfo() : local_wi;
+	return true;
+}
+
+bool VulkanHostDisplay::InitializeRenderDevice(std::string_view shader_cache_directory, bool debug_device)
+{
+	Vulkan::ShaderCache::Create(shader_cache_directory, SHADER_CACHE_VERSION, debug_device);
+	return true;
+}
+
+bool VulkanHostDisplay::HasRenderDevice() const
+{
+	return static_cast<bool>(g_vulkan_context);
+}
+
+bool VulkanHostDisplay::HasRenderSurface() const
+{
+	return static_cast<bool>(m_swap_chain);
+}
+
+bool VulkanHostDisplay::CreateImGuiContext()
+{
+	ImGui_ImplVulkan_InitInfo vii = {};
+	vii.Instance = g_vulkan_context->GetVulkanInstance();
+	vii.PhysicalDevice = g_vulkan_context->GetPhysicalDevice();
+	vii.Device = g_vulkan_context->GetDevice();
+	vii.QueueFamily = g_vulkan_context->GetGraphicsQueueFamilyIndex();
+	vii.Queue = g_vulkan_context->GetGraphicsQueue();
+	vii.PipelineCache = g_vulkan_shader_cache->GetPipelineCache();
+	vii.MinImageCount = m_swap_chain->GetImageCount();
+	vii.ImageCount = m_swap_chain->GetImageCount();
+	vii.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+
+	return ImGui_ImplVulkan_Init(&vii, m_swap_chain->GetClearRenderPass());
+}
+
+void VulkanHostDisplay::DestroyImGuiContext()
+{
+	g_vulkan_context->WaitForGPUIdle();
+	ImGui_ImplVulkan_Shutdown();
+}
+
+bool VulkanHostDisplay::UpdateImGuiFontTexture()
+{
+	// Just in case we were drawing something.
+	g_vulkan_context->ExecuteCommandBuffer(true);
+	ImGui_ImplVulkan_DestroyFontUploadObjects();
+	return ImGui_ImplVulkan_CreateFontsTexture(g_vulkan_context->GetCurrentCommandBuffer());
+}
+
+void VulkanHostDisplay::DestroyRenderDevice()
+{
+	if (!g_vulkan_context)
+		return;
+
+	g_vulkan_context->WaitForGPUIdle();
+
+	Vulkan::ShaderCache::Destroy();
+	DestroyRenderSurface();
+	Vulkan::Context::Destroy();
+}
+
+bool VulkanHostDisplay::MakeRenderContextCurrent()
+{
+	return true;
+}
+
+bool VulkanHostDisplay::DoneRenderContextCurrent()
+{
+	return true;
+}
+
+bool VulkanHostDisplay::BeginPresent(bool frame_skip)
+{
+	if (frame_skip || !m_swap_chain || ShouldSkipDisplayingFrame())
+	{
+		if (ImGui::GetCurrentContext())
+			ImGui::Render();
+
+		g_vulkan_context->ExecuteCommandBuffer(false);
+		return false;
+	}
+
+	// Previous frame needs to be presented before we can acquire the swap chain.
+	g_vulkan_context->WaitForPresentComplete();
+
+	VkResult res = m_swap_chain->AcquireNextImage();
+	if (res != VK_SUCCESS)
+	{
+		if (res == VK_SUBOPTIMAL_KHR || res == VK_ERROR_OUT_OF_DATE_KHR)
+		{
+			ResizeRenderWindow(0, 0, m_window_info.surface_scale);
+			res = m_swap_chain->AcquireNextImage();
+		}
+		else if (res == VK_ERROR_SURFACE_LOST_KHR)
+		{
+			Console.Warning("Surface lost, attempting to recreate");
+			if (!m_swap_chain->RecreateSurface(m_window_info))
+			{
+				Console.Error("Failed to recreate surface after loss");
+				g_vulkan_context->ExecuteCommandBuffer(false);
+				m_swap_chain.reset();
+				return false;
+			}
+
+			res = m_swap_chain->AcquireNextImage();
+		}
+
+		// This can happen when multiple resize events happen in quick succession.
+		// In this case, just wait until the next frame to try again.
+		if (res != VK_SUCCESS && res != VK_SUBOPTIMAL_KHR)
+		{
+			// Still submit the command buffer, otherwise we'll end up with several frames waiting.
+			LOG_VULKAN_ERROR(res, "vkAcquireNextImageKHR() failed: ");
+			g_vulkan_context->ExecuteCommandBuffer(false);
+			return false;
+		}
+	}
+
+	VkCommandBuffer cmdbuffer = g_vulkan_context->GetCurrentCommandBuffer();
+	const Vulkan::Util::DebugScope debugScope(cmdbuffer, "VulkanHostDisplay::BeginPresent");
+
+	// Swap chain images start in undefined
+	Vulkan::Texture& swap_chain_texture = m_swap_chain->GetCurrentTexture();
+	swap_chain_texture.OverrideImageLayout(VK_IMAGE_LAYOUT_UNDEFINED);
+	swap_chain_texture.TransitionToLayout(cmdbuffer, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL);
+
+	const VkClearValue clear_value = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
+	const VkRenderPassBeginInfo rp = {VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
+		nullptr,
+		m_swap_chain->GetClearRenderPass(),
+		m_swap_chain->GetCurrentFramebuffer(),
+		{{0, 0}, {swap_chain_texture.GetWidth(), swap_chain_texture.GetHeight()}},
+		1u,
+		&clear_value};
+	vkCmdBeginRenderPass(g_vulkan_context->GetCurrentCommandBuffer(), &rp, VK_SUBPASS_CONTENTS_INLINE);
+	Vulkan::Util::SetViewportAndScissor(cmdbuffer, 0, 0, swap_chain_texture.GetWidth(), swap_chain_texture.GetHeight());
+	return true;
+}
+
+void VulkanHostDisplay::EndPresent()
+{
+	if (ImGui::GetCurrentContext())
+	{
+		const Vulkan::Util::DebugScope debugScope(g_vulkan_context->GetCurrentCommandBuffer(), "Imgui");
+		ImGui::Render();
+		ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), g_vulkan_context->GetCurrentCommandBuffer());
+	}
+
+	VkCommandBuffer cmdbuffer = g_vulkan_context->GetCurrentCommandBuffer();
+	vkCmdEndRenderPass(g_vulkan_context->GetCurrentCommandBuffer());
+	m_swap_chain->GetCurrentTexture().TransitionToLayout(cmdbuffer, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR);
+
+	g_vulkan_context->SubmitCommandBuffer(m_swap_chain->GetImageAvailableSemaphore(),
+		m_swap_chain->GetRenderingFinishedSemaphore(), m_swap_chain->GetSwapChain(),
+		m_swap_chain->GetCurrentImageIndex(), !m_swap_chain->IsVSyncEnabled());
+	g_vulkan_context->MoveToNextCommandBuffer();
+}
+
+HostDisplay::AdapterAndModeList VulkanHostDisplay::StaticGetAdapterAndModeList(const WindowInfo* wi)
+{
+	AdapterAndModeList ret;
+	std::vector<Vulkan::SwapChain::FullscreenModeInfo> fsmodes;
+
+	if (g_vulkan_context)
+	{
+		ret.adapter_names = Vulkan::Context::EnumerateGPUNames(g_vulkan_context->GetVulkanInstance());
+		if (wi)
+		{
+			fsmodes = Vulkan::SwapChain::GetSurfaceFullscreenModes(g_vulkan_context->GetVulkanInstance(),
+				g_vulkan_context->GetPhysicalDevice(), *wi);
+		}
+	}
+	else if (Vulkan::LoadVulkanLibrary())
+	{
+		ScopedGuard lib_guard([]() { Vulkan::UnloadVulkanLibrary(); });
+
+		VkInstance instance = Vulkan::Context::CreateVulkanInstance(nullptr, false, false);
+		if (instance != VK_NULL_HANDLE)
+		{
+			ScopedGuard instance_guard([&instance]() { vkDestroyInstance(instance, nullptr); });
+
+			if (Vulkan::LoadVulkanInstanceFunctions(instance))
+				ret.adapter_names = Vulkan::Context::EnumerateGPUNames(instance);
+		}
+	}
+
+	if (!fsmodes.empty())
+	{
+		ret.fullscreen_modes.reserve(fsmodes.size());
+		for (const Vulkan::SwapChain::FullscreenModeInfo& fmi : fsmodes)
+		{
+			ret.fullscreen_modes.push_back(GetFullscreenModeString(fmi.width, fmi.height, fmi.refresh_rate));
+		}
+	}
+
+	return ret;
+}
